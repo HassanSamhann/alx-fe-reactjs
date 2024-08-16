3161f7094be704db453667dcb1ff3e3b74963385
hassan <hassan700019@gmail.com>
2024-08-16 18:04:47 +0300

    gg

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,17 @@
-
+import React from "react";
+import UserContext from "./UserContext";
+    
 function UserProfile(props) {
   return (
+    <UserContext.Provider >
     <div>
     <h2>{props.name}</h2>
     <p>Age: {props.age}</p>
     <p>Bio: {props.bio}</p>
   </div>
+
+  </UserContext.Provider>
+
 
   )
 }
