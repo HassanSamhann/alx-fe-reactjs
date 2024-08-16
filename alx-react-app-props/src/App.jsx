5cd4f431d5c099332d017f71eeb7dfd246488ee4
hassan <hassan700019@gmail.com>
2024-08-16 17:48:58 +0300

    co mit

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,19 +1,14 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import MainContent from "./components/MainContent";
-import UserProfile from "./components/UserProfile";
-import "./App.css";
-import WelcomeMessage from "./components/WelcomeMessage";
+import ProfilePage from './ProfilePage';
+import {UserContext} from './components/UserContext';
 function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  
   return (
-    <>
-      <Header />
-      <MainContent />
-      <Footer />
-      <WelcomeMessage />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-    </>
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
   );
 }
 
-export default App;
+
+export default App; 
