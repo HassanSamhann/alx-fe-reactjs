fdbcba3d3328d856f185b902316a82acf45ea2de
hassan <hassan700019@gmail.com>
2024-08-25 20:39:49 +0300

    wdw

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 
 const RecipeList = () => {
   const filteredRecipes = useRecipeStore(state => state.filteredRecipes);
+  const recipes = useRecipeStore((state) => state.recipes);
 
   return (
     <div>
