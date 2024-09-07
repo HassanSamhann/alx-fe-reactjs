0443ff05cde794cfc563b725229c43f4bab65a8d
hassan <hassan700019@gmail.com>
2024-09-07 15:55:52 +0300

    gg

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,7 +1,9 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <div className="align-middle">
+    <h1 className="text-3xl font-bold underline text-cyan-500 text-center ">
       Hello world!
     </h1>
+    </div>
   )
 }
