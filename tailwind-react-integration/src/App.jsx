4da14c907772475fcb19cef5189d5cee37a40ddd
hassan <hassan700019@gmail.com>
2024-09-07 16:08:03 +0300

    rr

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,9 +1,14 @@
+import UserProfile from "./components/UserProfile";
+
 export default function App() {
   return (
     <div className="align-middle">
     <h1 className="text-3xl font-bold underline text-cyan-500 text-center ">
       Hello world!
     </h1>
+
+      <UserProfile/>
+
     </div>
   )
 }
