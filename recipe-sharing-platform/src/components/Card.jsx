8b87c98fdf2827ed6e19f3d62b0bf90f64161258
hassan <hassan700019@gmail.com>
2024-09-11 11:07:44 +0300

    qw

--- recipe-sharing-platform/src/components/Card.jsx
+++ recipe-sharing-platform/src/components/Card.jsx
@@ -5,7 +5,7 @@ function Card(props) {
     <div className="card xl:w-1/5 overflow-hidden  border-2 border-cyan-500 my-5 shadow-xl rounded-md sm:w-full flex flex-col gap-3 items-center hover:shadow-2xl transition ">
       <img className="w-40 hover:scale-110 transition" src={reCard.image} alt="" />
       <h2 className="text-center text-black font-bold">{reCard.title}</h2>
-      <p className="text-center text-black">{reCard.description}</p>
+      <p className="text-center text-black">{reCard.summary}</p>
     </div>
   )
 }
