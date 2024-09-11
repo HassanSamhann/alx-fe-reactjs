77230bf8fc2d27da10a70d1d271e7f0b8ea8725c
hassan <hassan700019@gmail.com>
2024-09-11 11:12:09 +0300

    fwe

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -17,11 +17,22 @@ function HomePage() {
     <div className="flex justify-between flex-wrap gap-6 mx-10">
     {resdata}
     </div>
-    <div className="card xl:w-1/5 overflow-hidden  border-2 border-cyan-500 my-5 shadow-xl rounded-md sm:w-full flex flex-col gap-3 items-center hover:shadow-2xl transition ">
-      <img className="w-40 hover:scale-110 transition" src={} alt="" />
+    <div className="card xl:w-1/5 overflow-hidden  border-2 grid   grid-cols-3 border-cyan-500 my-5 shadow-xl rounded-md sm:w-full  flex-col gap-3 items-center hover:shadow-2xl transition ">
+      <img className="w-40 hover:scale-110 transition" src="" alt="" />
       <h2 className="text-center text-black font-bold">{}</h2>
       <p className="text-center text-black">{}</p>
     </div>
+    <div className="card xl:w-1/5 overflow-hidden  border-2 grid grid-cols-1  border-cyan-500 my-5 shadow-xl rounded-md sm:w-full  flex-col gap-3 items-center hover:shadow-2xl transition ">
+      <img className="w-40 hover:scale-110 transition" src="" alt="" />
+      <h2 className="text-center text-black font-bold">{}</h2>
+      <p className="text-center text-black">{}</p>
+    </div>
+    <div className="card xl:w-1/5 overflow-hidden  border-2 grid grid-cols-2  border-cyan-500 my-5 shadow-xl rounded-md sm:w-full  flex-col gap-3 items-center hover:shadow-2xl transition ">
+      <img className="w-40 hover:scale-110 transition" src="" alt="" />
+      <h2 className="text-center text-black font-bold">{}</h2>
+      <p className="text-center text-black">{}</p>
+    </div>
+
   </>);
 }
 
