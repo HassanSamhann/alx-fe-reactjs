7734d555cd474a100e34e35ecf683c6e0b439db5
hassan <hassan700019@gmail.com>
2024-09-11 11:09:12 +0300

    er

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -17,6 +17,11 @@ function HomePage() {
     <div className="flex justify-between flex-wrap gap-6 mx-10">
     {resdata}
     </div>
+    <div className="card xl:w-1/5 overflow-hidden  border-2 border-cyan-500 my-5 shadow-xl rounded-md sm:w-full flex flex-col gap-3 items-center hover:shadow-2xl transition ">
+      <img className="w-40 hover:scale-110 transition" src={} alt="" />
+      <h2 className="text-center text-black font-bold">{}</h2>
+      <p className="text-center text-black">{}</p>
+    </div>
   </>);
 }
 
