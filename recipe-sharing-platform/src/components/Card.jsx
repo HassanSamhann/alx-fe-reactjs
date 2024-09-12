d586dd27e84556d139fcb6d334a45a26dee210f7
hassan <hassan700019@gmail.com>
2024-09-12 13:57:16 +0300

    change

--- recipe-sharing-platform/src/components/Card.jsx
+++ recipe-sharing-platform/src/components/Card.jsx
@@ -1,11 +1,12 @@
-
+import {Link} from 'react-router-dom'
 function Card(props) {
    let {reCard} = props;  
   return (
-    <div className="card xl:w-1/5 overflow-hidden  border-2 border-cyan-500 my-5 shadow-xl rounded-md sm:w-full flex flex-col gap-3 items-center hover:shadow-2xl transition ">
-      <img className="w-40 hover:scale-110 transition" src={reCard.image} alt="" />
+    <div className="card xl:w-1/5 overflow-hidden  border-2 border-cyan-500 my-5 shadow-xl rounded-md sm:w-full flex flex-col justify-between items-center hover:shadow-2xl transition ">
+      <img className="w-40 hover:scale-110 transition my-2" src={reCard.image} alt="" />
       <h2 className="text-center text-black font-bold">{reCard.title}</h2>
       <p className="text-center text-black">{reCard.summary}</p>
+      <Link className="px-10 py-3 bg-cyan-500 hover:bg-cyan-700 rounded-lg transition mb-2   "  to={`/recipe/${reCard.id}`} >more details</Link>
     </div>
   )
 }
---
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState  } from "react";
+import { useParams } from "react-router-dom";
+import Card from "./Card";
+    
+    function RecipeDetail() {
+     let { id } = useParams()
+
+        let [proudct,SetProudct] = useState([]) ; 
+
+        let api = "../data.json" ; 
+
+
+        useEffect(() =>{
+            fetch(`${api}`)
+           .then((res) => res.json())
+           .then((data) => SetProudct(data))
+        },[])   
+
+        let prom  = proudct.map((pro ,index)=> pro.id === parseInt(id) &&  <Card key={index}  reCard={pro} /> ) ; 
+      return (
+      <>
+        {prom}
+       
+        </>
+      )
+    }
+    
+    export default RecipeDetail ; 
