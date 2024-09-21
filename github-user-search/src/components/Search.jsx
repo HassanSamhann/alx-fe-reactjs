24b3940e786f9f4b881a6ede0d2ddb228ad4c03c
hassan <hassan700019@gmail.com>
2024-09-21 16:36:11 +0300

    vw

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -4,6 +4,9 @@ import  { useState } from 'react';
 
 const Search = ({ onSearch }) => {
   const [username, setUsername] = useState('');
+  const [location, setLocation] = useState('');
+  const [repos, setRepos] = useState('');
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -14,15 +17,31 @@ const Search = ({ onSearch }) => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="Enter GitHub username"
-        />
-        <button type="submit">Search</button>
-      </form>
+     <form onSubmit={handleSubmit} className="flex flex-col gap-4 p-6 bg-white shadow-md rounded-md" >
+  <input 
+  className="p-2 border rounded"
+    type="text"
+    placeholder="Enter GitHub username"
+    value={username}
+    onChange={(e) => setUsername(e.target.value)}
+  />
+  <input
+  className="p-2 border rounded"
+    type="text"
+    placeholder="Location"
+    value={location}
+    onChange={(e) => setLocation(e.target.value)}
+  />
+  <input
+  className="p-2 border rounded"
+    type="number"
+    placeholder="Minimum Repositories"
+    value={repos}
+    onChange={(e) => setRepos(e.target.value)}
+  />
+  <button type="submit">Search</button>
+</form>
+
     </div>
   );
 };
